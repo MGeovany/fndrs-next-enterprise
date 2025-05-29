5dba26cca96a4b16b93dc9cfb773230909f87a65
Marlon Geovany Castro <marlongeo1999@gmail.com>
2025-05-29 16:55:43 -0600

    chore: update prettier configuration for improved formatting and import sorting

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,15 @@
+/** @type {import("prettier").Config} */
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  trailingComma: "es5",
-  tabWidth: 2,
-  printWidth: 120,
   semi: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  bracketSameLine: true,
+  bracketSpacing: true,
+  printWidth: 130,
+  arrowParens: "always",
+  endOfLine: "auto",
+  plugins: ["prettier-plugin-tailwindcss", "@ianvs/prettier-plugin-sort-imports"],
+  importOrder: ["<BUILT_IN_MODULES>", "", "<THIRD_PARTY_MODULES>", "", "^[^@./]", "", "^[./]"],
 }
