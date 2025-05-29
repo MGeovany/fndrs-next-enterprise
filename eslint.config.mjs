9bc48f01ceb5771016847394060dbff51ead5fae
Marlon Geovany Castro <marlongeo1999@gmail.com>
2025-05-29 11:56:19 -0600

    chore: update ESLint configuration and dependencies
    - Refactored ESLint configuration to include TypeScript parser options and removed unused import resolver settings.
    - Added `eslint-import-resolver-typescript` as a dependency to enhance TypeScript support in ESLint.
    - Updated `pnpm-lock.yaml` to reflect the addition of the new resolver and other dependency updates.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,24 @@
-import * as fs from "fs"
 import eslintPluginNext from "@next/eslint-plugin-next"
 // https://github.com/francoismassart/eslint-plugin-tailwindcss/pull/381
 // import eslintPluginTailwindcss from "eslint-plugin-tailwindcss"
-import eslintPluginImport from "eslint-plugin-import"
 import eslintPluginStorybook from "eslint-plugin-storybook"
 import typescriptEslint from "typescript-eslint"
 
 const eslintIgnore = [".git/", ".next/", "node_modules/", "dist/", "build/", "coverage/", "*.min.js", "*.config.js", "*.d.ts"]
-
-const config = typescriptEslint.config(
+typescriptEslint.config(
   {
-    ignores: eslintIgnore
+    ignores: eslintIgnore,
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json"
+      }
+    }
   },
+
   ...eslintPluginStorybook.configs["flat/recommended"],
   //  https://github.com/francoismassart/eslint-plugin-tailwindcss/pull/381
   // ...eslintPluginTailwindcss.configs["flat/recommended"],
   typescriptEslint.configs.recommended,
-  eslintPluginImport.flatConfigs.recommended,
   {
     plugins: {
       "@next/next": eslintPluginNext
@@ -30,16 +32,6 @@ const config = typescriptEslint.config(
     settings: {
       tailwindcss: {
         callees: ["classnames", "clsx", "ctl", "cn", "cva"]
-      },
-
-      "import/resolver": {
-        typescript: {
-          alwaysTryTypes: true,
-          project: "./tsconfig.json"
-        },
-        node: {
-          extensions: [".js", ".jsx", ".ts", ".tsx"]
-        }
       }
     },
     rules: {
@@ -48,13 +40,6 @@ const config = typescriptEslint.config(
         {
           argsIgnorePattern: "^_",
           varsIgnorePattern: "^_"
-        }
-      ],
-      "sort-imports": [
-        "error",
-        {
-          ignoreCase: true,
-          ignoreDeclarationSort: true
         }
       ]
     }
